b2de27913c40f094279ca5d5aa24567d7ef3fedd
John Mettraux <jmettraux@gmail.com>
2010-02-15 23:35:19 +0900

    moved to sinatra

--- start.rb
+++ start.rb
@@ -1,46 +1,23 @@
 
 #
-# just a tiny Ruby Rack application for serving the stuff in public/
+# just a tiny Sinatra application for serving the stuff in public/
 #
 # start with
 #
 #   ruby start.rb
 #
-# (don't forget to
-#
-#   sudo gem install rack mongrel
-#
-# if necessary)
-#
 
 require 'rubygems'
-require 'rack'
+require 'sinatra'
 
-class App
-  def initialize
-    @rfapp = Rack::File.new('public')
-  end
-  def call (env)
-    env['PATH_INFO'] = '/index.html' if env['PATH_INFO'] == '/'
-    @rfapp.call(env)
-  end
-end
+use Rack::CommonLogger
+use Rack::ShowExceptions
 
-b = Rack::Builder.new do
+set :public, File.expand_path(File.join(File.dirname(__FILE__), 'public'))
+#set :views, File.expand_path(File.join(File.dirname(__FILE__), 'views'))
 
-  use Rack::CommonLogger
-  use Rack::ShowExceptions
-  run App.new
-end
+get '/' do
 
-port = 4567
-
-puts ".. [#{Time.now}] listening on port #{port}"
-
-Rack::Handler::Mongrel.run(b, :Port => port) do |server|
-  trap(:INT) do
-    puts ".. [#{Time.now}] stopped."
-    server.stop
-  end
+  redirect '/index.html'
 end
 
